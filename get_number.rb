75980e8e308c4d4c9cbfc29bce7ddd5b3eaac8a3
Suleyman Musayev <slmusayev@gmail.com>
2022-01-25 09:13:58 +0500

    update get_number.rb to obtain player name

--- get_number.rb
+++ get_number.rb
@@ -1,9 +1,9 @@
 #Get My Number Game
 # Written by msuliq
 
+#Getting player name and putting on the greeting
 puts "Welcome to 'Get My Number!"
 print "What's your name?"
-
 input = gets
-
-puts "Welcome, #{input}"
+name.input.chomp
+puts "Welcome, #{input}!"
