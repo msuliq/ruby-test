fe52a0c5c46895591a206020f3139685dc44d614
Suleyman Musayev <slmusayev@gmail.com>
2022-01-27 13:39:08 +0500

    add self. in print_payslip line 28 in payslip.rb

--- payslip.rb
+++ payslip.rb
@@ -20,15 +20,16 @@ class Employee
 
     #sets default name and salary for new employees as part of failsafe
     def initialize(name = "Anonymous", salary = 0.0)
-        @name = name
-        @salary = salary
+        #prevents blank names and negative salary in default input
+        self.name = name
+        self.salary = salary
     end
 
     #printing of the payslip
     def print_payslip
-        puts "Name: #{@name}"
+        puts "Name: #{self.name}"
         #calculation of actual payable amount for 14 days
-        pay_for_period = (@salary / 365.0) * 14
+        pay_for_period = (self.salary / 365.0) * 14
         #formatting of calculated result to include 2 decimals
         formatted_pay = format("%.2f", pay_for_period)
         puts "Pay for this period $#{formatted_pay}"
@@ -36,12 +37,4 @@ class Employee
 end
 
 #data input about employees
-amy = Employee.new
-amy.name = "Amy Blake"
-amy.salary = 45000
-
-#runs in terminal
-amy.print_payslip
-
-employee = Employee.new
-employee.print_payslip
+Employee.new("Amy Blake", 45000).print_payslip
