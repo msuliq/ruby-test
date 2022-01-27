5a144ae75193c34630913a86d8654fd747c5ba70
Suleyman Musayev <slmusayev@gmail.com>
2022-01-27 13:58:16 +0500

    add hourly employees to payslip.rb

--- payslip.rb
+++ payslip.rb
@@ -19,7 +19,7 @@ class Employee
     end
 end
 
-#SalariedEmployees whose pay is calculated based on 365 days
+#Employees with annual salary whose pay is calculated based on 365 days
 class SalariedEmployee < Employee
     attr_reader :salary
     #give error message if salary is a negative number
@@ -47,8 +47,46 @@ class SalariedEmployee < Employee
     end   
 end
 
+#Employees with hourly pay whose payslip is calculated based on
+#hours per week
+class HourlyEmployee < Employee
+    attr_reader :hourly_wage, :hours_per_week
+
+    #give error message if wage is a negative number
+    def hourly_wage=(hourly_wage)
+       if hourly_wage < 0
+            raise "An hourly wage of #{hourly_wage} is not valid!"
+        end
+        @hourly_wage = hourly_wage
+    end
+    
+    #give error message if hours per week are a negative number
+    def hours_per_week=(hours_per_week)
+        if hours_per_week < 0
+            raise "#{hours_per_week} hours per week are not valid."
+        end
+        @hours_per_week = hours_per_week
+    end
+
+    #printing of payslip for hourly employees
+    def print_payslip
+        puts "Name: #{name}"
+        #calculation of actual payable amount for 2 weeks
+        pay_for_period = hourly_wage * hours_per_week * 2
+        #formatting of calculated result to include 2 decimals
+        formatted_pay = format("%.2f", pay_for_period)
+        puts "Pay for this period $#{formatted_pay}"
+    end
+end
+
 #data input about employees
 salaried_employee = SalariedEmployee.new
 salaried_employee.name = "Jane Doe"
 salaried_employee.salary = 36000
 salaried_employee.print_payslip
+
+hourly_employee = HourlyEmployee.new
+hourly_employee.name = "John Doe"
+hourly_employee.hourly_wage = 14.99
+hourly_employee.hours_per_week = 30
+hourly_employee.print_payslip
