c6e001d7b8ac87a679d970d83f7b1dd3405fe431
Suleyman Musayev <slmusayev@gmail.com>
2022-02-01 14:36:55 +0500

    update wordsplitter and add more enumerable methods

--- wordsplitter.rb
+++ wordsplitter.rb
@@ -24,10 +24,23 @@ splitter.each do |word|
 end
 
 #methods enabled by Enumerable
+#find all words matching parameter
 p splitter.find_all { |word| word.include?("d") }
+#reject all words matching parameter
 p splitter.reject { |word| word.include?("d") }
+#create array with words in reverse or of other param
 p splitter.map { |word| word.reverse }
+#return true if at least one element matches param
 p splitter.any? { |word| word.include?("e") }
+#count all elements in array
 p splitter.count
+#return first element in array
 p splitter.first
+# sort elements in array
 p splitter.sort
+#group elements by param
+p splitter.group_by { |word|  word.include?("do") }
+#find word with max length
+p splitter.max_by { |word| word.length }
+#get array of all words
+p splitter.to_a
