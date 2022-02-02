1c5c3263fc59468e5dd127dcebecab01278f823a
Suleyman Musayev <slmusayev@gmail.com>
2022-02-02 17:03:12 +0500

    add test file for ListWithCommas.rb

---
+++ test/TestListWithCommas.rb
@@ -0,0 +1,25 @@
+#script for automated testing of ListWithCommas.rb
+
+#load minitest module
+require 'minitest/autorun'
+#load the test class
+require 'list_with_commas'
+
+class TestListWithCommas < Minitest::Test
+    #test of joining two subjects
+    def test_it_joins_two_words_with_and
+        list = ListWithCommas.new
+        #two sample subjects for the test
+        list.items = ['apple', 'orange']
+        #test is passed if join returns the required line
+        assert('apple and orange' = list.join)
+    end
+    #test of joining three subjects
+    def test_it_joins_three_words_with_commas
+        list = ListWithCommas.new
+        #three sample subjects for the test
+        list = ['apple', 'orange', 'pear']
+        #test passed if join return same line
+        assert('apple, orange, and pear' = list.join)
+    end
+end
