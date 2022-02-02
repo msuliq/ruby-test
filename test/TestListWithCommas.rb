a239a00c8e63b0ac1ee85b0c67fe6e1f383a3384
Suleyman Musayev <slmusayev@gmail.com>
2022-02-02 17:25:50 +0500

    remove duplicating code from TestListWithCommas.rb

--- test/TestListWithCommas.rb
+++ test/TestListWithCommas.rb
@@ -6,29 +6,31 @@ require 'minitest/autorun'
 require 'listwithcommas'
 
 class TestListWithCommas < Minitest::Test
+    #setup of testing conditions
+    def setup
+        #create ListWithCommas.new var for all test instances
+        @list = ListWithCommas.new
+    end 
     #test of joining one subject
     def test_it_prints_one_word_alone
-        list = ListWithCommas.new
         #single item for test run
-        list.items = ['apple']
+        @list.items = ['apple']
         #comparison of test run and list join result
         #if true - test pass, if false - test fail
-        assert_equal('apple', list.join)
+        assert_equal('apple', @list.join)
     end
     #test of joining two subjects
     def test_it_joins_two_words_with_and
-        list = ListWithCommas.new
         #two sample subjects for the test
-        list.items = ['apple', 'orange']
+        @list.items = ['apple', 'orange']
         #test is passed if join returns the required line
-        assert_equal('apple and orange', list.join)
+        assert_equal('apple and orange', @list.join)
     end
     #test of joining three subjects
     def test_it_joins_three_words_with_commas
-        list = ListWithCommas.new
         #three sample subjects for the test
-        list = ['apple', 'orange', 'pear']
+        @list = ['apple', 'orange', 'pear']
         #test passed if join return same line
-        assert_equal('apple, orange, and pear', list.join)
+        assert_equal('apple, orange, and pear', @list.join)
     end
 end
