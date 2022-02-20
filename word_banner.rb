d7cd781dbeb60773627b9f4755d4b220fe3bcd95
Suleyman Musayev <slmusayev@gmail.com>
2022-02-20 22:29:47 +0500

    add emoji animation in terminal

--- word_banner.rb
+++ word_banner.rb
@@ -1,6 +1,18 @@
 #code to print text in terminal slowly letter by letter
-
-"hello world!".chomp.split(//).map(&:upcase).each do |c|
+"how does this code work?".split(//).map(&:upcase).each do |c|
     sleep 0.3
     print c
+end
+#adds a new line to print the emoji
+puts ""
+#prints two ascii emojis as terminal animation
+shrug = Enumerator.new do |e|
+    loop do
+      e.yield '^|_(ツ)_|^'
+      e.yield '¯\\_(ツ)_/¯'
+    end
+  end
+  1.upto(100) do |i|
+    printf("\r %s", shrug.next)
+    sleep(0.3)
 end
